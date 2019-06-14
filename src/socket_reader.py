438e551edbada358bb7f90bf087ea9b1174725dc
vietbq <vietbq@nal.vn>
2019-06-15 01:02:21 +0700

    Update

--- src/socket_reader.py
+++ src/socket_reader.py
@@ -5,12 +5,11 @@ import threading
 import io
 import detection as detection
 
-class SocketReader(threading.Thread):
 
-    def __init__(self, connection_lock, connection):
+class SocketReader(threading.Thread):
+    def __init__(self, connection):
         super(SocketReader, self).__init__()
         self.connection = connection
-        self._lock = connection_lock
         self.stream = io.BytesIO()
         self.terminated = False
         self.start()
@@ -18,16 +17,15 @@ class SocketReader(threading.Thread):
     def run(self):
         while not self.terminated:
             try:
-                with self._lock:
-                    print('Waiting for read stream')
-                    data_len = struct.unpack('<L', self.connection.read(struct.calcsize('<L')))[0]
-                    if not data_len:
-                        continue
-                    self.stream.write(self.connection.read(data_len))
-                    text_obj = self.stream.decode('UTF-8')
-                    print(text_obj)
-                    self.stream.seek(0)
-                    self.stream.truncate()
+                print('Waiting for read stream')
+                data_len = struct.unpack('<L', self.connection.read(struct.calcsize('<L')))[0]
+                if not data_len:
+                    continue
+                self.stream.write(self.connection.read(data_len))
+                text_obj = self.stream.decode('UTF-8')
+                print(text_obj)
+                self.stream.seek(0)
+                self.stream.truncate()
             except Exception as e:
                 print(e)
                 print('Reader disconnected')
