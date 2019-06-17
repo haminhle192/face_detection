e022011bd58b925ce315751361fdbc50cfb4bc2a
vietbq <vietbq@nal.vn>
2019-06-17 23:30:21 +0700

    Update

--- src/gpio_manager.py
+++ src/gpio_manager.py
@@ -3,20 +3,25 @@ __author__ = 'vietbq'
 import RPi.GPIO as GPIO
 import time
 
-light_1_pin = 10
+light_1_pin = 12
 light_2_pin = 11
 
 class GPIO_Manager:
 
     def __init__(self):
         GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
-        GPIO.setup(light_1_pin, GPIO.OUT) # LED pin set as output
-        GPIO.setup(light_2_pin, GPIO.OUT) # PWM pin set as output
+        GPIO.setup(light_1_pin, GPIO.OUT)
+        GPIO.setup(light_2_pin, GPIO.OUT)
+        GPIO.output(light_1_pin, GPIO.LOW)
+        GPIO.output(light_2_pin, GPIO.LOW)
         self.start_on= 0
+        self.on = False
+        print('GPIO Manager initialed')
 
     def turn_on(self):
-        self.start_on = time.time()
-        GPIO.output(light_1_pin, GPIO.HIGH)
+        if not self.on:
+            self.start_on = time.time()
+            GPIO.output(light_1_pin, GPIO.HIGH)
 
     def turn_off(self):
         GPIO.output(light_1_pin, GPIO.LOW)
