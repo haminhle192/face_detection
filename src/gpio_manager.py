186720275b18b0270fef30c1f93b5f7247748c16
vietbq <vietbq@nal.vn>
2019-06-17 23:44:17 +0700

    Update

--- src/gpio_manager.py
+++ src/gpio_manager.py
@@ -3,17 +3,13 @@ __author__ = 'vietbq'
 import RPi.GPIO as GPIO
 import time
 
-light_1_pin = 11
-light_2_pin = 12
+light_1_pin = 18
 
 class GPIO_Manager:
-
     def __init__(self):
         GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
         GPIO.setup(light_1_pin, GPIO.OUT)
-        GPIO.setup(light_2_pin, GPIO.OUT)
         GPIO.output(light_1_pin, GPIO.LOW)
-        GPIO.output(light_2_pin, GPIO.LOW)
         self.start_on= 0
         self.on = False
         print('GPIO Manager initialed')
